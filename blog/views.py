a4d744f70dc76d7294b425c951ae88b98d46ceef
sniper <NGTDimka@gmail.com>
2017-10-11 14:20:02 +0500

    commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from blog.models import Post
+from django.views.generic import ListView, DetailView
+
+class PostsListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
